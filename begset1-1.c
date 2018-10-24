9f020e453cec3fc5658990d84511a711e0c9b0dd
Aman-Gupta67 <43518653+Aman-Gupta67@users.noreply.github.com>
2018-10-25 02:16:03 +0530

    Update begset1-1.c

--- begset1-1.c
+++ begset1-1.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+void main(){
+  
+int a;
+scanf("Enter a number %d",&a);
+if (a>0){
+  printf("Positive Number");
+}
+  else if(a<0){
+    printf("negative number");
+  }
+  else
+    printf("Zero");
+}
