b47c0d168458cc4a3f694437d763ae40205cf75c
Aman-Gupta67 <43518653+Aman-Gupta67@users.noreply.github.com>
2018-11-08 17:43:31 +0530

    Update begset1-2.c

--- begset1-2.c
+++ begset1-2.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
 void main(){
-  
-int a;
-scanf("Enter a number %d",&a);
-if (a%2==0){
-  printf("Even Number");
-}
-  else if(a%2!=0){
-    printf("Odd number");
-  }
-  else
-    printf("Invalid input");
+    char cha;
+    printf("Enter a character:");
+    scanf("%c",&cha);
+    if ((cha>='a'&&cha<='z')||(cha>='A'&&cha<='Z')){
+    printf("alphabet");}
+    else{printf("no");}
 }
