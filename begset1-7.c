3391bcc3ea51b4420302f2a5d93fd7689874000b
Aman-Gupta67 <43518653+Aman-Gupta67@users.noreply.github.com>
2018-11-08 21:40:30 +0530

    Update and rename begset1-6.c to begset1-7.c

---
+++ begset1-7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int y;
+    printf("Enter a number of times:");
+    scanf("%d ",&y);
+        for (int i=0; i<y;i++){
+    printf("Hello");
+        printf("\n");
+   
+        }
+   
+}
